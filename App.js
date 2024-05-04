67d2818162eb7f7590d2e115b3a71eb2e81e49f4
Shashank <shashankshekhar746@gmail.com>
2024-05-04 22:08:17 +0530

    Jsx

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+//React.createElement() => React Element - JS Object => HTMLElements(render)
+
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "Namaste React"
+// );
+
+// JSX- HTML like or XML-like JSX
+
+// JSX(transpiled before it reaches JS engine) - PARCEL -Babel
+
+// JSX => React.createelement
+
+const jsxHeading = <h1 id="heading">Namaste react using JSX</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading)
 
 
-const parent  = React.createElement('div',
-    {id:"parent"},
-   [React.createElement('div',{id:'child1'},
-   [React.createElement('h1',{},'I am child1 H1 tag'),
-   React.createElement('h2',{},"Im H2"),
-]),
-React.createElement('div',{id:'child2'},
-   [React.createElement('h1',{},'I am child2 H1 tag'),
-   React.createElement('h2',{},"Im H2"),
-])
-  
-]);
-
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
--- readme.md
+++ readme.md
@@ -2,4 +2,22 @@
 Repo for learning React
 =======
 Repo for learning React 
->>>>>>> 19989fa7c92061308a809d81873416561f85ee4b
+>>>>>>>
+
+#Parcel
+-Dev Build
+-local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - written in C++
+-Faster builds because of caching 
+-Image Optimization
+-Minification
+-Bundling
+-Compress 
+-Consisting Routing
+-Code Spliting
+-Differential bundling - support older browser
+- Diagnostic
+-Error handling
+-HTPPs-
+-Tree Shaking - remobe umused code
