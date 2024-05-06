ff344183135e7f9ae10b1642c895f4bd9f6c208f
Shashank <shashankshekhar746@gmail.com>
2024-05-06 21:49:55 +0530

    Notes Updated

--- App.js
+++ App.js
@@ -13,12 +13,24 @@ import ReactDOM from "react-dom/client"
 
 // JSX(transpiled before it reaches JS engine) - PARCEL -Babel
 
-// JSX => React.createelement
+// JSX => babel transpiles it React.createelement
 
-const jsxHeading = <h1 id="heading">Namaste react using JSX</h1>
+const Title = ()=> (
+<h1  className="head">
+    Namaste react using JSX
+</h1>
+)
 
+//React Component 
+
+const HeadingComponent = ()=>(
+    <div id="container">
+    <Title />
+        <h2 className="heading">Hello World! using React component</h2>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading)
+root.render(<HeadingComponent />)
 
 
